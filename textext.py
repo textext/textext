5b9b14f2719dcff5366d179cd59ef3c97a5f7890
Pauli Virtanen <pav@iki.fi>
2008-05-09 20:20:42 +0300

    Bump version

--- textext.py
+++ textext.py
@@ -43,7 +43,7 @@ contributions.
 
 #------------------------------------------------------------------------------
 
-__version__ = "0.4.1.dev"
+__version__ = "0.4.2"
 __docformat__ = "restructuredtext en"
 
 import sys, os, glob, traceback
