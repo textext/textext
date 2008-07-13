0e12527272f9aba9ebde3dc2e8da35ca999b09f0
Pauli Virtanen <pav@iki.fi>
2008-07-13 20:21:15 +0300

    Back to development

--- textext.py
+++ textext.py
@@ -43,7 +43,7 @@ for contributions.
 
 #------------------------------------------------------------------------------
 
-__version__ = "0.4.3"
+__version__ = "0.4.3.dev"
 __docformat__ = "restructuredtext en"
 
 import sys, os, glob, traceback, platform
