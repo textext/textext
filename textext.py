ddf6b55ad9744ff42f3de61f0475ce147ebf3e60
Pauli Virtanen <pauli@ltl.tkk.fi>
2009-05-15 12:14:00 +0300

    Bump version to dev
    --HG--
    branch : 0.3.x

--- textext.py
+++ textext.py
@@ -41,7 +41,7 @@ but is now rewritten.
 
 #------------------------------------------------------------------------------
 
-__version__ = "0.3.4"
+__version__ = "0.3.dev"
 __author__ = "Pauli Virtanen <pav@iki.fi>, Robert Szalai <szalai@mm.bme.hu>"
 __docformat__ = "restructuredtext en"
 
