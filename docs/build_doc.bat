fb596c9b3ff87a7a0b4a7d30c0642227ffb594c2
Jan Winkler <enceladus@posteo.de>
2016-12-23 10:45:01 +0100

    Fixed/ Changed: No pics in html-doc. Prictures moved into the doc dir.
    Pictures have been moved into the doc-dir for proper display in html, md and
    pdf documentation. As a consequence windows bat-scripts for building the
    script had to be modified so pandoc sees the correct directories during
    compilation.

---
+++ docs/build_doc.bat
@@ -0,0 +1,5 @@
+@echo off
+
+pandoc --verbose --variable linkcolor=blue --variable fontfamily=opensans --variable fontfamilyoptions=default,osfigures,scale=0.95 --variable margin-left=2cm --variable margin-right=2cm --variable margin-top=2.5cm --variable margin-bottom=2.5cm --variable fontsize=12pt Readme.md -o Readme.pdf
+
+pandoc --verbose -s -S -c pandoc.css Readme.md -o Readme.html
