949323c07b80ca1eb6a9a9f52a7ccaa3c221df80
Jan Winkler <enceladus@posteo.de>
2017-01-19 22:20:09 +0100

    Added: win_app_paths.py -> Module for safest possible detection of paths
    A module has been added which allows TexText to detect the correct paths
    to gs, pstoedit and imagamagick on 32-bit and 64-bit Windows machines
    with 32-bit or 64-bit Inkscape installed and 32-bit and 64-bit versions of
    gs, pstoedit and imagemagick.

---
+++ tests/win_app_paths/test_win_app_path.py
@@ -0,0 +1,43 @@
+import win_app_paths as wap
+
+res = wap.check_command(['C:\\Program Files (x86)\\gs\\gs9.14\\bin\\gswin32c.exe', '--help'])
+if res:
+    print('GS OK')
+else:
+    print('GS not found!')
+
+res = wap.check_command(['C:\\Program Files (x86)\\pstoedit\\pstoedit.exe', '-v'])
+if res:
+    print('pstoedit OK')
+else:
+    print('pstoedit not found!')
+
+res = wap.check_command(['pdflatex', '--help'])
+if res:
+    print('pdflatex OK')
+else:
+    print('pdflatex not found!')
+
+res = wap.check_command(['c:\\Program Files (x86)\\ImageMagick-6.8.8-Q16\\convert.exe', '--version'])
+if res:
+    print('ImageMagick OK')
+else:
+    print('ImageMagick not found!')
+
+dirname = wap.get_pstoedit_dir()
+if not dir:
+    print(wap.get_last_error())
+else:
+    print(dirname)
+
+dirname = wap.get_imagemagick_dir()
+if not dir:
+    print(wap.get_last_error())
+else:
+    print(dirname)
+
+dirname = wap.get_ghostscript_dir()
+if not dir:
+    print(wap.get_last_error())
+else:
+    print(dirname)
