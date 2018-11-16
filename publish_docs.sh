e03944748fc42b41aa5f1fb8c11f74d2f3bb4287
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2018-11-16 07:16:07 +0300

    Add script to publish gh-pages

---
+++ publish_docs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
+
+TARGET_BRANCH="gh-pages"
+GH_PAGES_ROOT="$PWD/docs/build/html"
+REPO=`git config remote.origin.url`
+SSH_REPO="${REPO/https:\/\/github.com\//git@github.com:}"
+SHA=`git rev-parse --verify HEAD`
+WD="$PWD"
+
+function doCompile {
+  make -C ./docs html
+}
+
+rm -rf "$GH_PAGES_ROOT" || exit 0
+git clone "$REPO" "$GH_PAGES_ROOT"
+rm -rf "$GH_PAGES_ROOT"/* || exit 0
+cd "$GH_PAGES_ROOT"
+git checkout --orphan "$TARGET_BRANCH"
+touch .nojekyll
+cd "$WD"
+
+doCompile
+
+cd "$GH_PAGES_ROOT"
+
+git add -A .
+git commit -m "Deploy to GitHub Pages: ${SHA}"
+git push "$SSH_REPO" --force "$TARGET_BRANCH"
