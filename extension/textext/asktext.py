d80b60d18bb0fead0657fdd4b25732a0f1b5f112
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2018-12-13 14:16:51 +0300

    Feature/add scroll preview mode (#93)
    Sometime output of TexText is too big for scaled preview.
    
    The PR add "scaled" mode to zoom in and see fine details of generated image.
    
    To switch between preview modes double click on preview area.
    
    The default view is always "scaled". Don't think we need to store this option anywhere.
    
    * feat(preview): wrap preview image in scroll window
    
    * feat(preview): switch between scaled and scrolled preview on dblclick
    
    Double click on preview image will switch between two modes.
    At first preview always appears in scaled mode.
    
    * feat(preview): add tooltip to preview image
    
    * feat(preview): disable switch between scale modes for small images
    
    Both modes appears same if preview images is small. Disable switch to
    avoid user confusion
    
    * feat(preview): set minimum source_view height to 150px
    
    Preview area was able to shrink source view area to single text line.

--- extension/textext/asktext.py
+++ extension/textext/asktext.py
@@ -471,11 +471,15 @@ if TOOLKIT in (GTK, GTKSOURCEVIEW):
     class AskTextGTKSource(AskText):
         """GTK + Source Highlighting for editing TexText objects"""
 
+
         def __init__(self, version_str, text, preamble_file, global_scale_factor, current_scale_factor, current_alignment,
                      current_texcmd, tex_commands, gui_config):
             super(AskTextGTKSource, self).__init__(version_str, text, preamble_file, global_scale_factor, current_scale_factor,
                                                    current_alignment, current_texcmd, tex_commands, gui_config)
-            self._preview = None
+            self._preview = None  # type: gtk.Image
+            self._pixbuf = None  # type: gtk.gdk.Pixbuf
+            self.preview_representation = "SCALE"  # type: str
+            self._preview_scroll_window = None  # type: gtk.ScrolledWindow
             self._scale_adj = None
             self._texcmd_cbox = None
             self._preview_callback = None
@@ -734,25 +738,76 @@ if TOOLKIT in (GTK, GTKSOURCEVIEW):
             Set the preview image in the GUI, scaled to the text view's width
             :param path: the path of the image
             """
-            textview_width = self._source_view.get_allocation().width
+
+            self._pixbuf = gtk.gdk.pixbuf_new_from_file(path)
+            self._preview_scroll_window.set_has_tooltip(False)
+            self.update_preview_representation()
+
+
+        def switch_preview_representation(self, widget=None, event=None):
+            if event.button == 1: # left click only
+                if event.type == gtk.gdk._2BUTTON_PRESS:  # only double click
+                    if self.preview_representation == "SCALE":
+                        if self._preview_scroll_window.get_has_tooltip():
+                            self.preview_representation = "SCROLL"
+                    else:
+                        if self._preview_scroll_window.get_has_tooltip():
+                            self.preview_representation = "SCALE"
+                    self.update_preview_representation()
+
+        def update_preview_representation(self):
+
             max_preview_height = 150
 
-            pixbuf = gtk.gdk.pixbuf_new_from_file(path)
-            image_width = pixbuf.get_width()
-            image_height = pixbuf.get_height()
-            scale = 1
+            textview_width = self._source_view.get_allocation().width
+            image_width = self._pixbuf.get_width()
+            image_height = self._pixbuf.get_height()
 
-            if image_width > textview_width:
-                scale = min(scale, (textview_width * 1.0 / image_width))
+            def set_scaled_preview():
+                scale = 1
+                if image_width > textview_width:
+                    scale = min(scale, (textview_width * 1.0 / image_width))
 
-            if image_height > max_preview_height:
-                scale = min(scale, (max_preview_height * 1.0 / image_height))
+                if image_height > max_preview_height:
+                    scale = min(scale, (max_preview_height * 1.0 / image_height))
 
-            if scale != 1:
-                pixbuf = pixbuf.scale_simple(int(image_width * scale), int(image_height * scale),
-                                             gtk.gdk.INTERP_BILINEAR)
+                pixbuf = self._pixbuf
+                if scale != 1:
+                    pixbuf = self._pixbuf.scale_simple(int(image_width * scale), int(image_height * scale),
+                                                                                  gtk.gdk.INTERP_BILINEAR)
+                    self._preview_scroll_window.set_tooltip_text("Double click: scale to original size")
 
-            self._preview.set_from_pixbuf(pixbuf)
+                self._preview.set_from_pixbuf(pixbuf)
+                self._preview.set_size_request(pixbuf.get_width(), pixbuf.get_height())
+
+                return image_height
+
+            def set_scroll_preview():
+
+                self._preview.set_size_request(image_width, image_height)
+
+                scroll_bar_width = 30
+
+                desired_preview_area_height = image_height
+                if image_width + scroll_bar_width >= textview_width:
+                    desired_preview_area_height += scroll_bar_width
+
+                if desired_preview_area_height>max_preview_height or image_width > textview_width:
+                    self._preview_scroll_window.set_tooltip_text("Double click: scale to fit window")
+
+                self._preview.set_from_pixbuf(self._pixbuf)
+                self._preview.set_size_request(image_width, image_height)
+                return desired_preview_area_height
+
+            if self.preview_representation == "SCROLL":
+                desired_preview_area_height = set_scroll_preview()
+            else:
+                desired_preview_area_height = set_scaled_preview()
+
+            self._preview_scroll_window.set_size_request(-1, min(desired_preview_area_height, max_preview_height))
+            self._preview_scroll_window.show()
+
+
 
         # ---------- create view window
         def create_buttons(self):
@@ -949,6 +1004,7 @@ if TOOLKIT in (GTK, GTKSOURCEVIEW):
             self._source_buffer = text_buffer
             self._source_view = source_view
             self._source_buffer.set_text(self.text)
+            self._source_view.set_size_request(-1, 150)
 
             scroll_window.add(self._source_view)
             set_monospace_font(self._source_view)
@@ -977,6 +1033,20 @@ if TOOLKIT in (GTK, GTKSOURCEVIEW):
 
             # latex preview
             self._preview = gtk.Image()
+            self._preview_scroll_window = gtk.ScrolledWindow()
+            self._preview_scroll_window.set_shadow_type(gtk.SHADOW_NONE)
+            self._preview_scroll_window.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+            preview_viewport = gtk.Viewport()
+            preview_viewport.set_shadow_type(gtk.SHADOW_NONE)
+            preview_viewport.add(self._preview)
+            self._preview_scroll_window.add(preview_viewport)
+
+            preview_event_box = gtk.EventBox()
+            preview_event_box.add_events(gtk.gdk.BUTTON_PRESS_MASK)
+
+            preview_event_box.connect('button-press-event', self.switch_preview_representation)
+            preview_event_box.add(self._preview_scroll_window)
+
 
             # Vertical Layout
             vbox = gtk.VBox(False, 4)
@@ -989,10 +1059,11 @@ if TOOLKIT in (GTK, GTKSOURCEVIEW):
 
             vbox.pack_start(scroll_window, True, True, 0)
             vbox.pack_start(pos_label, False, False, 0)
-            vbox.pack_start(self._preview, False, False, 0)
+            vbox.pack_start(preview_event_box, False, False, 0)
             vbox.pack_start(self.create_buttons(), False, False, 0)
 
             vbox.show_all()
+            self._preview_scroll_window.hide()
 
             # preselect menu check items
             groups = ui_manager.get_action_groups()
