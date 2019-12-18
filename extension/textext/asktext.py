36f3465451550330dbe5c0fe214b8971d1d722be
Jan Winkler <enceladus@posteo.de>
2019-12-18 14:06:10 +0100

    Removed unnecessary 'None' check for close shortcut

--- extension/textext/asktext.py
+++ extension/textext/asktext.py
@@ -726,12 +726,11 @@ if TOOLKIT in (GTK, GTKSOURCEVIEW):
 
             # Cancel dialog via shortcut if set by the user
             close_shortcut_value = self._gui_config.get("close_shortcut", self.DEFAULT_CLOSE_SHORTCUT)
-            if close_shortcut_value != 'None':
-                if (close_shortcut_value == 'Escape' and Gdk.keyval_name(event.keyval) == 'Escape') or \
-                   (close_shortcut_value == 'CtrlQ' and Gdk.keyval_name(event.keyval) == 'q' and
-                    Gdk.ModifierType.CONTROL_MASK and event.state):
-                    self._cancel_button.clicked()
-                    return True
+            if (close_shortcut_value == 'Escape' and Gdk.keyval_name(event.keyval) == 'Escape') or \
+               (close_shortcut_value == 'CtrlQ' and Gdk.keyval_name(event.keyval) == 'q' and
+                Gdk.ModifierType.CONTROL_MASK and event.state):
+                self._cancel_button.clicked()
+                return True
 
             return False
 
