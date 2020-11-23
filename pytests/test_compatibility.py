7802a64ba1ca60f45c0ed2220ce1ea8006a3ede7
Jan <enceladus@posteo.de>
2020-11-23 12:49:37 +0100

    CI: Suppressed annoying svg output

--- pytests/test_compatibility.py
+++ pytests/test_compatibility.py
@@ -219,7 +219,7 @@ def is_current_version_compatible(test_id,
             r"--scale-factor=%f" % mod_args["scale-factor"],
             r"--preamble-file=%s" % mod_args["preamble-file"],
             r"--alignment=%s" % mod_args["alignment"],
-            svg_original])
+            svg_original], output=os.devnull)
 
         svg2 = os.path.join(tmp_dir, "svg2.svg")
 
