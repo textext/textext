8282f7dc2a587cb3d3646c632fddec905c5c863a
Jan Winkler <enceladus@posteo.de>
2023-06-08 17:13:32 +0200

    Added core window using new Inkscape API
    We make heavy use of the inskcape gtk extension API
    for Windows management.

---
+++ textext2/textext.py
@@ -0,0 +1,49 @@
+__version__ = "2.0"
+__pkgname__ = "textext"
+
+import os
+import warnings
+warnings.filterwarnings("ignore")
+
+import inkex
+from inkex.gui import GtkApp, Window
+
+
+class TextextGui(Window):
+
+    name = "textext_gui"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def on_btn_preview_clicked(self, widget=None):
+        pass
+
+    def on_btn_execute_clicked(self, widget=None):
+        pass
+
+    def on_btn_cancel_clicked(self, widget=None):
+        pass
+
+    def on_btn_scalereset_clicked(self, widget=None):
+        pass
+
+    def on_win_main_destroy(self, widget=None):
+        pass
+
+
+class TexTextApp(GtkApp):
+
+    glade_dir = os.path.join(os.path.dirname(__file__))
+    app_name = "textext"
+    windows = [TextextGui]
+
+
+class TexText(inkex.EffectExtension):
+
+    def effect(self):
+        TexTextApp(start_loop=True)
+
+
+if __name__ == "__main__":
+    TexText().run()
