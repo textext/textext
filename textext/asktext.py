89c046e4fa55c3cb2b55564736fdd269628ca43b
Jan Winkler <enceladus@posteo.de>
2023-07-26 10:40:15 +0200

    Updated year in copyright notices

--- textext/asktext.py
+++ textext/asktext.py
@@ -2,7 +2,7 @@
 This file is part of TexText, an extension for the vector
 illustration program Inkscape.
 
-Copyright (c) 2006-2022 TexText developers.
+opyright (c) 2006-2023 TexText developers.
 
 TexText is released under the 3-Clause BSD license. See
 file LICENSE.txt or go to https://github.com/textext/textext
